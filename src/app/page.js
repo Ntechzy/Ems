ea930ab0e69b16d29eec0c8dd0272426c004b82a
pankaj upadhyay <pankajup.git01@gmail.com>
2024-09-24 15:52:04 +0530

    color and input added

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,27 @@
-import React from 'react'
+'use client'
+import Input from '@/component/Input'; 
+import React, { useState } from 'react'
 
 const page = () => {
+  const [data, setData] = useState({
+    Name: "",
+    Email: "",
+    Mobile_Number: "",
+    Applying_for: "B.D.S",
+  });
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.id]: e.target.value });
+  };
   return (
-    <div>page</div>
+    <div className='bg-[#1d6ba3] h-[100vh] flex justify-center items-center m-auto'>
+      <Input
+        name={"Name"}
+        label={"Name"}
+        handleChange={handleChange}
+        value={data.Name}
+      />
+    </div>
   )
 }
 

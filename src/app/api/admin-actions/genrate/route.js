f295aee40b310facc5763cc7112860d8c7928b89
pankaj-Ntechzy <pankaj@ntechzy.in>
2024-11-14 16:47:39 +0530

    upload file change

--- src/app/api/admin-actions/genrate/route.js
+++ src/app/api/admin-actions/genrate/route.js
@@ -46,16 +46,15 @@ export async function POST(req, res) {
                 resolve(html);
             });
         });
-        console.log("ok  ");
+        const response = await axios.post("https://selectpdf.com/api2/convert", {
+            html,
+            key: "419e2aef-0c63-42e2-bcc2-fa5675ae4eb7"
+        }, { responseType: 'arraybuffer' }); // Expect an ArrayBuffer response
 
-        const browser = await puppeteer.launch({
-            headless: "new", // Alternatively, you can set it to true if "new" does not work for you
-            args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
-        });
-        console.log("ok niche aaya hai ");
-        
-        const page = await browser.newPage();
-        await page.setContent(html);
+        const pdfBuffer = Buffer.from(response.data);
+        // const browser = await puppeteer.launch({ headless: true });
+        // const page = await browser.newPage();
+        // await page.setContent(html);
 
         // const pdfBuffer = await page.pdf({ format: 'A4' });
         // await browser.close();

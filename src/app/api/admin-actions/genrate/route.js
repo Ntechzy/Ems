c3471bd42e89e95eee3c5184e04aa76af6748ce3
Ntechzy <ntechzy@gmail.com>
2024-11-14 13:55:39 +0530

    logged error

--- src/app/api/admin-actions/genrate/route.js
+++ src/app/api/admin-actions/genrate/route.js
@@ -94,7 +94,7 @@ export async function POST(req, res) {
             );
         }
     } catch (error) {
-        console.log(error);
+        console.log("Error:", error);
         return Response.json({ success: false, message: error, }, { status: 500 });
     }
 }

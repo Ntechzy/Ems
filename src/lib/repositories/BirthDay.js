3a52027a397aa6404e07ef697cab69d70c41fc9a
pankaj-Ntechzy <pankaj@ntechzy.in>
2025-02-20 15:07:02 +0530

    birthday bug resolved

--- src/lib/repositories/BirthDay.js
+++ src/lib/repositories/BirthDay.js
@@ -1,34 +1,44 @@
 import { Base } from "./Base";
 import BirthDayModel from '../../modal/birthday'
 
-export class BirthDay extends Base{
+export class BirthDay extends Base {
     #modelName;
-    constructor(){
+    constructor() {
         super(BirthDayModel)
         this.#modelName = BirthDayModel;
     }
 
-    async Update(filter , dataToUpdate){
-        const res = await this.#modelName.findOneAndUpdate(filter , dataToUpdate , {upsert:true,new:true});
+    async Update(filter, dataToUpdate) {
+        const res = await this.#modelName.findOneAndUpdate(filter, dataToUpdate, { upsert: true, new: true });
         return res;
     }
 
     async GetAllTodayBirthdays() {
         const today = new Date();
-    
-        const startOfDay = new Date(today.getFullYear(), today.getMonth(), today.getDate());
-        const endOfDay = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
-    
-        const todayBirthdays = await this.#modelName.find({
-            date: {
-                $gte: startOfDay,
-                $lt: endOfDay    
+        const currentMonth = today.getMonth() + 1;
+        const currentDay = today.getDate();
+
+        const todayBirthdays = await this.#modelName.aggregate([
+            {
+                $project: {
+                    month: { $month: "$date" },
+                    day: { $dayOfMonth: "$date" },
+                    name: 1,
+                    date: 1
+                }
+            },
+            {
+                $match: {
+                    month: currentMonth,
+                    day: currentDay
+                }
             }
-        });
-    
+        ]); 
+
         return todayBirthdays;
     }
-    
+
+
 
 
 }
